a59303f37536de0ab7f2f9c720e96b8340c041cc
Celian Garcia <celian.garcia1@gmail.com>
2017-05-01 10:44:28 +0200

    firefly_api

--- firefly_core/src/server/ResponseBuilder.cpp
+++ firefly_core/src/server/ResponseBuilder.cpp
@@ -6,6 +6,10 @@ namespace firefly {
     void ResponseBuilder::build(nlohmann::json json_content, std::shared_ptr<HttpResponse> response) {
         std::string content = json_content.dump();
         *response << "HTTP/1.1 200 \r\n";
+        *response << "Access-Control-Allow-Origin: *\r\n";
+        *response << "Access-Control-Allow-Methods: GET, POST, DELETE, PUT, PATCH, OPTIONS\r\n";
+        *response << "Access-Control-Allow-Headers: Content-Type, api_key, Authorization\r\n";
+        *response << "Content-Type: application/json\r\n";
         *response << "Content-Length: " << content.length() << "\r\n\r\n";
         *response << content;
     }
