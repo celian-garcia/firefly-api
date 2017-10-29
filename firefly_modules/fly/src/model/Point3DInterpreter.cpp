a324ee2e09eb85286bf1fc7477d4a784824ce5c1
Celian Garcia <celian.garcia1@gmail.com>
2017-10-29 10:47:08 +0100

    Refactor Point3DInterpreter

--- firefly_modules/fly/src/model/Point3DInterpreter.cpp
+++ firefly_modules/fly/src/model/Point3DInterpreter.cpp
@@ -4,32 +4,39 @@
 
 #include "fly_module/model/Point3DInterpreter.hpp"
 
-firefly::fly_module::Point3DInterpreter::Point3DInterpreter(PGresult *result) {
-    this->interpreter = new PGResultInterpreter(result);
-    this->interpreter->registerProperties(PROPERTIES());
-}
+namespace firefly {
+    namespace fly_module {
+        Point3DInterpreter::Point3DInterpreter(PGresult *result) {
+            this->interpreter = new PGResultInterpreter(result);
+            this->interpreter->registerProperties(PROPERTIES());
+        }
 
-firefly::fly_module::Point3DInterpreter::~Point3DInterpreter() {
-    delete this->interpreter;
-}
+        Point3DInterpreter::~Point3DInterpreter() {
+            delete this->interpreter;
+        }
 
-firefly::fly_module::Point3DBean firefly::fly_module::Point3DInterpreter::getPoint3D(int row) {
-    // Interpretation of the given row
-    auto id = this->interpreter->get<int>("id", row);
-    auto cloud_id = this->interpreter->get<int>("cloud_id", row);
-    auto value = this->interpreter->get<cv::Vec3f>("value", row);
-    auto operations = this->interpreter->get<std::vector<int>>("operations", row);
+        Point3DBean
+        Point3DInterpreter::getPoint3D(int row) {
+            // Interpretation of the given row
+            auto id = this->interpreter->get<int>("id", row);
+            auto cloud_id = this->interpreter->get<int>("cloud_id", row);
+            auto value = this->interpreter->get<cv::Vec3f>("value", row);
+            auto operations = this->interpreter->get<std::vector<int>>("operations", row);
 
-    // Point creation from interpreted data
-    Point3DBean point(id, value, cloud_id, operations);
+            // Point creation from interpreted data
+            Point3DBean point(id, value, cloud_id, operations);
 
-    return point;
-}
+            return point;
+        }
 
-int firefly::fly_module::Point3DInterpreter::get_row_number() {
-    return this->interpreter->get_row_number();
-}
+        int
+        Point3DInterpreter::get_row_number() {
+            return this->interpreter->get_row_number();
+        }
 
-const std::vector<const char *> firefly::fly_module::Point3DInterpreter::PROPERTIES() {
-    return {"id", "cloud_id", "value", "operations"};
+        const std::vector<const char *>
+        Point3DInterpreter::PROPERTIES() {
+            return {"id", "cloud_id", "value", "operations"};
+        }
+    }
 }
