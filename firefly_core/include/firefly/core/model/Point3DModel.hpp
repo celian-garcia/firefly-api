5e9daa0e2194dfb60b1cb8170f8e389e392dd0d4
Celian Garcia <celian.garcia1@gmail.com>
2017-11-05 10:14:15 +0100

    code style

--- firefly_core/include/firefly/core/model/Point3DModel.hpp
+++ firefly_core/include/firefly/core/model/Point3DModel.hpp
@@ -11,25 +11,23 @@
 #include "firefly/core/model/beans/Point3DBean.hpp"
 
 namespace firefly {
-    namespace fly_module {
 
-        class Point3DModel : public BaseModel {
-        public:
-            using BaseModel::BaseModel;
+class Point3DModel : public BaseModel {
+ public:
+    using BaseModel::BaseModel;
 
-            Point3DBean getPointByValueAndTaskId(cv::Vec3f value, int cloud_id);
+    Point3DBean getPointByValueAndTaskId(cv::Vec3f value, int cloud_id);
 
-            std::vector <Point3DBean> getPointListByTaskId(int task_id);
+    std::vector<Point3DBean> getPointListByTaskId(int task_id);
 
-            void insertPoint(Point3DBean point);
+    void insertPoint(Point3DBean point);
 
-            void insertOperation(Operation operation, int cloud_id);
+    void insertOperation(Operation operation, int cloud_id);
 
-            void updatePoint(Point3DBean point);
+    void updatePoint(Point3DBean point);
 
-            static constexpr double INTERSECT_MIN_DISTANCE = 0.00001;
-        };
+    static constexpr double INTERSECT_MIN_DISTANCE = 0.00001;
+};
 
-    }  // namespace fly_module
 }  // namespace firefly
 #endif  // INCLUDE_FIREFLY_MODEL_POINT3DMODEL_HPP_
--- firefly_core/include/firefly/core/model/beans/Point3DBean.hpp
+++ firefly_core/include/firefly/core/model/beans/Point3DBean.hpp
@@ -7,40 +7,38 @@
 #include <opencv2/opencv.hpp>
 
 namespace firefly {
-    namespace fly_module {
 
-        class Point3DBean {
-        public:
+class Point3DBean {
+ public:
 
-            Point3DBean(cv::Vec3f value,
-                                 int cloud_id, std::vector<int> operations_ids);
+    Point3DBean(cv::Vec3f value,
+                int cloud_id, std::vector<int> operations_ids);
 
-            Point3DBean(int id, cv::Vec3f value,
-                                 int cloud_id, std::vector<int> operations_ids);
+    Point3DBean(int id, cv::Vec3f value,
+                int cloud_id, std::vector<int> operations_ids);
 
-            int const &getId() const;
+    int const &getId() const;
 
-            cv::Vec3f const &getValue() const;
+    cv::Vec3f const &getValue() const;
 
-            int const &getTaskId() const;
+    int const &getTaskId() const;
 
-            std::vector<int> const &getOperationsIds() const;
+    std::vector<int> const &getOperationsIds() const;
 
-            void setValue(const cv::Vec3f &value);
+    void setValue(const cv::Vec3f &value);
 
-            void setOperationsIds(const std::vector<int> &operations_ids);
+    void setOperationsIds(const std::vector<int> &operations_ids);
 
-            void setTaskId(const int &task_id);
+    void setTaskId(const int &task_id);
 
-            void addOperationId(const int &operation_id);
+    void addOperationId(const int &operation_id);
 
-        private:
-            int m_id = 0;
-            cv::Vec3f m_value;
-            int m_task_id;
-            std::vector<int> m_operations_ids;
-        };
+ private:
+    int m_id = 0;
+    cv::Vec3f m_value;
+    int m_task_id;
+    std::vector<int> m_operations_ids;
+};
 
-    }  // namespace fly_module
 }  // namespace firefly
 #endif  // INCLUDE_FIREFLY_MODEL_BEANS_POINT3DBEAN_HPP_
