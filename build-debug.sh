9a73b3793bca9ee9e4dad0dd12aea7e8816be1ad
Celian Garcia <celian.garcia1@gmail.com>
2017-11-09 08:34:33 +0100

    remove unused build scripts

--- build-debug.sh
+++
@@ -1,8 +0,0 @@
-rm -rf build
-mkdir -p build/debug
-cd build/debug && cmake \
-    -G "MSYS Makefiles" \
-    -DCMAKE_CXX_COMPILER="C:/MinGW/bin/g++.exe" \
-    -DCMAKE_C_COMPILER="C:/MinGW/bin/gcc.exe" \
-    -DFly_DIR="C:/Users/celia/Development/projects/fly" \
-    ../..
--- build_project.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -rf build;
-ls -l;
-mkdir build;
-cd build;
-ls -l;
-#cmake -DPostgreSQL_TYPE_INCLUDE_DIR=/usr/include/postgresql ..;
-#make -j4;
